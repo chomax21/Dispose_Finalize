5c12bec6a60df2a495e1e45ebff0b61347d668e9
chomax21 <maximcheb21@gmail.com>
2023-08-28 16:40:19 +0300

    Добавьте файлы проекта.

---
+++ Dispose_Finalize/TestClass.cs
@@ -0,0 +1,38 @@
+﻿namespace Dispose_Finalize
+{
+    internal class TestClass : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Hello()
+        {
+            Console.WriteLine("HELLO!!!");
+        }
+
+        public void Dispose()
+        {
+            // Free manage code resourse
+            CleanUp(true);
+            Console.Beep();
+        }
+
+        private void CleanUp(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    // Освободить управляемые ресурсы. Выполняется если вызвал пользователь объекта.
+                }
+                // Очистить не управляемые ресурсы. В любом случаее.
+            }
+            
+            disposed = true;
+        }
+        
+        ~TestClass()
+        { 
+            CleanUp(false);
+        }
+    }
+}
